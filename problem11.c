2ec0bf39111081c3d6a82ff5321c7a34bcdf1535
Varshini Saravanan <siva.varshinisaravanan@gmail.com>
2026-06-20 23:22:19 +0530

    Delete problem11.c

--- problem11.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-   printf("%zu\n", sizeof(int));
-   printf("%zu\n", sizeof(float));
-   printf("%zu\n", sizeof(double));
-   printf("%zu", sizeof(char));
-   return 0;
-}
